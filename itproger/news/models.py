61d3b30ccd92c6a4759368c4b5738bbd7df91bdc
Dmitry-pytho <dmitry.solomiannyi@gmail.com>
2022-08-09 22:19:14 +0300

    добавлена форма для добавления статей

--- itproger/news/models.py
+++ itproger/news/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Articles(models.Model):
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+
     title = models.CharField('Название', max_length=50)
     anons = models.CharField('Анонс', max_length=250)
     full_text = models.TextField('Статья')
@@ -10,4 +15,3 @@ class Articles(models.Model):
 
     def __str__(self):
         return self.title
-
--- itproger/news/views.py
+++ itproger/news/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+from .models import Articles
+
 # Create your views here.
 
 def news_home(request):
-    # return render(request, 'main/about.html')
-    return render(request, 'news/news_home.html')
+    news = Articles.objects.all()
+    return render(request, 'news/news_home.html', {"news": news})
+
+def create(request):
+    return render(request, 'news/create.html')
